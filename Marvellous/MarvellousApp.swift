dc3742ab884120fa85f7f0f4a3ef32d65689b5c8
Andria <andria.inas@gmail.com>
2024-02-28 18:18:33 +0400

    first commit

---
+++ Marvellous/MarvellousApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvellousApp.swift
+//  Marvellous
+//
+//  Created by Andria Inasaridze on 28.02.24.
+//
+
+import SwiftUI
+
+@main
+struct MarvellousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
